279a7dfd61f967dfcf98b8f0fd6369a1b7a4a84f
katarzynatrzaska <katarzynatrzaska90@gmail.com>
2020-01-13 21:43:58 +0100

    Page factory implemented

--- src/test/java/LoginPage.java
+++ src/test/java/LoginPage.java
@@ -1,6 +1,8 @@
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
 
@@ -20,23 +22,24 @@ public class LoginPage {
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
-    public void typeIntoUserNameField(String username){
+    public void typeIntoUserNameField(String username) {
         usernameField.clear();
         usernameField.sendKeys(username);
     }
 
-    public void typeIntoPasswordField(String password){
+    public void typeIntoPasswordField(String password) {
         passwordField.clear();
         passwordField.sendKeys(password);
     }
 
-    public void clickOnLoginButton(){
+    public void clickOnLoginButton() {
         signOnButton.click();
     }
 
-    public String getWarningMessage(){
+    public String getWarningMessage() {
         String warningText = messageLabel.getText();
         return warningText;
     }

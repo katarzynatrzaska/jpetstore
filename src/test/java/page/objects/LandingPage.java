bc07f16e6e3cd4465a4e153c72516d8fa0be2226
katarzynatrzaska <katarzynatrzaska90@gmail.com>
2020-01-14 19:28:31 +0100

    waits implemented

--- src/test/java/page/objects/LandingPage.java
+++ src/test/java/page/objects/LandingPage.java
@@ -1,9 +1,11 @@
 package page.objects;
 
+
 import driver.manager.DriverManager;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import waits.WaitForElement;
 
 public class LandingPage {
 
@@ -15,6 +17,7 @@ public class LandingPage {
     }
 
     public void clickOnEnterStoreLink() {
+        WaitForElement.waitUntilElementIsClickable(enterStoreLink);
         enterStoreLink.click();
     }
 

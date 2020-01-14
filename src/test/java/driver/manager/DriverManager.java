8caa201f205d9ab1d145937a94e6443c7eb7baa6
katarzynatrzaska <katarzynatrzaska90@gmail.com>
2020-01-14 18:02:56 +0100

    Driver manager and Driver Utils added

---
+++ src/test/java/driver/manager/DriverManager.java
@@ -0,0 +1,29 @@
+package driver.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+
+    private static WebDriver driver;
+
+    private DriverManager() {
+    }
+
+    public static WebDriver getWebDriver() {
+
+        if (driver == null) {
+            //System.setProperty("webdriver.chrome.driver", "C:/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+            driver = new ChromeDriver();
+        }
+
+        return driver;
+    }
+
+    public static void disposeDriver(){
+        driver.close();
+        driver.quit();
+        driver = null;
+    }
+}

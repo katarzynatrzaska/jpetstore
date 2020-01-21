df91aafa606ba57c7997cdc9afd44f86f26e1efe
katarzynatrzaska <katarzynatrzaska90@gmail.com>
2020-01-21 18:18:49 +0100

    Added logger

--- src/test/java/page/objects/FooterPage.java
+++ src/test/java/page/objects/FooterPage.java
@@ -1,12 +1,15 @@
 package page.objects;
 
 import driver.manager.DriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import waits.WaitForElement;
 
 public class FooterPage {
+
 
     @FindBy(css = "#Banner img[src*='dog']")
     private WebElement bannerAfterLoginLogo;
--- src/test/java/page/objects/LandingPage.java
+++ src/test/java/page/objects/LandingPage.java
@@ -7,8 +7,12 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import waits.WaitForElement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LandingPage {
+
+    private Logger logger = LogManager.getRootLogger();
 
     @FindBy(css = "#Content a")
     private WebElement enterStoreLink;
@@ -20,6 +24,8 @@ public class LandingPage {
     public void clickOnEnterStoreLink() {
        // WaitForElement.waitUntilElementIsClickable(enterStoreLink);
         enterStoreLink.click();
+        logger.info("Clicked on Enter Store link");
+
     }
 
 }
